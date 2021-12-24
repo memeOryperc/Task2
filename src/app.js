c8c8b274e528de4e0beb1c2b7621766d546f8ba9
aProkaten <81019615+memeOryperc@users.noreply.github.com>
2021-12-24 10:52:31 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const visitorRouter = require("./resources/visitor/visitor.router");
-const orderRouter = require("./resources/order/order.router");
-const bookRouter = require("./resources/book/book.router");
+import express from 'express';
+
+import swaggerUI from 'swagger-ui-express';
+import path from 'path';
+import YAML from 'yamljs';
+
+import consumerRouter from "./resources/visitor/visitor.router";
+import orderRouter from "./resources/order/order.router";
+import productRouter from "./resources/book/book.router";
 
 const app = express();
 
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
 app.use(express.json());
+
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') {
@@ -15,8 +24,8 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.use('/visitors', visitorRouter);
+app.use('/consumers', consumerRouter);
 app.use('/orders', orderRouter);
-app.use('/books', bookRouter);
+app.use('/products', productRouter);
 
-module.exports = app;
+export default app;
